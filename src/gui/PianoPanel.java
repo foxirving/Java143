3a6e84084cb7e8a7a32e70448e898040b95798dd
foxirving <amy.eli.irving@gmail.com>
2015-05-27 14:10:04 -0700

    New Gui version
    Signed-off-by: foxirving <amy.eli.irving@gmail.com>

--- src/gui/PianoPanel.java
+++ src/gui/PianoPanel.java
@@ -15,6 +15,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PianoPanel extends JPanel {
 
@@ -81,6 +83,66 @@ public class PianoPanel extends JPanel {
 		stylePanelTwo.setBackground(Color.CYAN);
 		stylePanelTwo.setBounds(0, 295, 800, 15);
 		pianoPanel.add(stylePanelTwo);
+		
+		JPanel PianoPanelTwo = new JPanel();
+		PianoPanelTwo.setBounds(85, 26, 630, 258);
+		pianoPanel.add(PianoPanelTwo);
+		PianoPanelTwo.setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("3");
+			}
+		});
+		panel_3.setBounds(44, 0, 83, 110);
+		PianoPanelTwo.add(panel_3);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("2");
+			}
+		});
+		panel_2.setBackground(Color.ORANGE);
+		panel_2.setBounds(84, 0, 85, 258);
+		PianoPanelTwo.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("1");
+			}
+		});
+		panel_1.setBackground(Color.GREEN);
+		panel_1.setBounds(0, 0, 85, 258);
+		PianoPanelTwo.add(panel_1);
+		panel_1.setLayout(null);
+//		
+//		JPanel panel_1 = new JPanel();
+//		panel_1.setBounds(47, 0, 90, 119);
+//		PianoPanelTwo.add(panel_1);
+//		panel_1.setLayout(null);
+//		
+//		JButton btnNewButton_1 = new JButton("New button");
+//		btnNewButton_1.setBounds(0, 0, 89, 119);
+//		panel_1.add(btnNewButton_1);
+//		
+//		JButton btnNewButton = new JButton("New button");
+//		btnNewButton.setBounds(0, 0, 90, 258);
+//		PianoPanelTwo.add(btnNewButton);
+//		
+//		JButton button = new JButton("New button");
+//		button.setBounds(91, 0, 90, 258);
+//		PianoPanelTwo.add(button);
+//		
+//		JButton button_1 = new JButton("New button");
+//		button_1.setBounds(176, 0, 90, 258);
+//		PianoPanelTwo.add(button_1);
 		
 		JPanel bottomPanel = new JPanel();
 		bottomPanel.setBounds(0, 390, 800, 50);
